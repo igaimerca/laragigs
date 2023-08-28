aa92175efa925ff0d4909a0400916a746325b711
igaimerca <igaimerca@gmail.com>
2023-08-28 10:29:07 +0200

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+  //    return view('welcome');
+  return response("<h1>Hello World</h1>", 200)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar');
+});
+Route::get('/posts/{id}', function ($id) {
+  //    ddd($id);
+  return response("Post " . $id);
+})->where('id', '[0-9]+');
+
+Route::get("/search", function (Request $request) {
+  return $request->name . ' ' . $request->city;
 });
