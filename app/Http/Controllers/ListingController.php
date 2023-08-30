41a20fca2bc18d46bf915c517c0fe6ef6e20ae89
igaimerca <igaimerca@gmail.com>
2023-08-30 11:15:30 +0200

    feat: login

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -31,7 +31,7 @@ class ListingController extends Controller
   }
   public function store(Request $request)
   {
-    // dd($request->all());
+    // dd($request->logo->store());
     $formFields = $request->validate([
       'title' => 'required',
       'company' => ['required', Rule::unique('listings', 'company')],
@@ -41,7 +41,39 @@ class ListingController extends Controller
       'tags' => 'required',
       'description' => 'required'
     ]);
+
+    if ($request->hasFile('logo')) {
+      $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
     Listing::create($formFields);
     return redirect("/")->with('message', 'Listing successfully created');
+  }
+
+  public function edit(Listing $listing)
+  {
+    return view('listings.edit', ['listing' => $listing]);
+  }
+  public function update(Request $request, Listing $listing)
+  {
+    $formFields = $request->validate([
+      'title' => 'required',
+      'company' => ['required'],
+      'location' => 'required',
+      'website' => 'required',
+      'email' => ['required', 'email'],
+      'tags' => 'required',
+      'description' => 'required'
+    ]);
+
+    if ($request->hasFile('logo')) {
+      $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+    $listing->update($formFields);
+    return back()->with('message', 'Listing updated successfully');
+  }
+  public function destroy(Listing $listing)
+  {
+    $listing->delete();
+    return redirect('/')->with('message', 'Listing deleted successfully');
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,9 +23,32 @@ Route::get('/', [ListingController::class, 'index']);
 // show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// get edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update the listing
+Route::put("/listings/{listing}/update", [ListingController::class, 'update']);
+
+// delete a listing
+Route::delete("/listings/{listing}", [ListingController::class, 'destroy']);
+
 // store listing
 Route::post('/listings', [ListingController::class, 'store']);
 
-
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// show register/create form
+Route::get('/register', [UserController::class, 'create']);
+
+// store/ create a new user
+Route::post('/users', [UserController::class, 'store']);
+
+// log user out
+Route::post('/logout', [UserController::class, 'logout']);
+
+// show login
+Route::get('/login', [UserController::class, 'login']);
+
+// login user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
